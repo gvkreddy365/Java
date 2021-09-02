1577277f98d8616517ea6ae1b89fa6cc32b25f4b
gvkreddy365 <gvkreddy365@gmail.com>
2021-09-02 18:02:36 +1000

    code for day 4

--- ClassvsInstance/src/Person.java
+++ ClassvsInstance/src/Person.java
@@ -1,47 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Person {
     private int age;
-
-    public Solution(int initialAge) {
+    public Person(int initialAge) {
         // Add some more code to run some checks on initialAge
-        if(initialAge<0)
-        {
-            System.out.println("Age is not valid, setting age to 0");
-        }
-        this.age=0;
-    }
 
-    public void amIOld() {
-        // Write code determining if this person's age is old and print the correct statement:
-        if(age<13)
-        {
-            System.out.println("You are young");
+        if (initialAge > 0) {
+            age = initialAge;
+        } else {
+            System.out.println("Age is not valid, setting age to 0.");
+            age = 0;
         }
-        else if(age>=13&&age<18)
+    }
+    public void amIOld() {
+        if (age < 13)
         {
-            System.out.println("You are teenager");
+            System.out.println("You are young.");
+        }
+        else if (age>=13&&age<18)
+        {
+            System.out.println("You are a teenager.");
         }
         else
         {
-            System.out.println("You are old");
+            System.out.println("You are old.");
         }
     }
-
     public void yearPasses() {
         // Increment this person's age.
         age++;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
+        for (int i = 0; i < T; i++)
+        {
             int age = sc.nextInt();
-            Solution p = new Solution(age);
+            Person p = new Person(age);
             p.amIOld();
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < 3; j++)
+            {
                 p.yearPasses();
             }
             p.amIOld();
